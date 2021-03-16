fa343da754e47014718bbfa4af687f37eb0cd9af
CodeKrakken <newholm1@hotmail.com>
2021-03-16 11:50:46 +0000

    Add header

---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react'
+import './Header.css'
+let applause = 'applause.mp3'
+
+export default class Header extends Component {
+  render() {
+    return (
+      <div id="header">
+        <h1 onClick={this.applause}>
+          Donald Newholm
+        </h1>
+        
+      </div>
+    )
+  }
+
+  applause = () => {
+    applause.play()
+  }
+}
